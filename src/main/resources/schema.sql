882fbd8ab64ba16236bfede246e888de5e74aa7b
Ilya Zaikov <Notherlight2@yandex.ru>
2019-09-26 14:54:20 +0300

    update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS Organization(
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
   name             VARCHAR(25) NOT NULL  COMMENT 'Имя организации',
   full_name        VARCHAR(50) NOT NULL  COMMENT 'Полное имя организации',
-  inn              BIGINT NOT NULL       COMMENT 'Идентификационный номер налогоплательщика',
-  kpp              BIGINT NOT NULL       COMMENT 'Код причины постановки на учёт',
+  inn              VARCHAR(15) NOT NULL  COMMENT 'Идентификационный номер налогоплательщика',
+  kpp              VARCHAR(15) NOT NULL  COMMENT 'Код причины постановки на учёт',
   address          VARCHAR(50) NOT NULL  COMMENT 'Адрес',
   phone            VARCHAR(15)           COMMENT 'Телефон',
   is_active        BIT NULL DEFAULT 1    COMMENT 'Активна ли органиация?'
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Office(
   phone            VARCHAR(15)           COMMENT 'Телефон',
   is_active        BIT NULL DEFAULT 1    COMMENT 'Активен ли офис?',
 
-  FOREIGN KEY (org_Id) REFERENCES Organization(id) COMMENT 'Внешний ключ на таблицу Организация'
+  FOREIGN KEY (org_id) REFERENCES Organization(id) COMMENT 'Внешний ключ на таблицу Организация'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS Doc_Type(
   id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
   name             VARCHAR(25) NOT NULL  COMMENT 'Имя документа',
-  doc_code         BIGINT                COMMENT 'Код документа'
+  doc_code         VARCHAR(15)           COMMENT 'Код документа'
  );
 COMMENT ON TABLE Doc_Type IS 'Тип документа';
 
@@ -49,24 +49,24 @@ CREATE TABLE IF NOT EXISTS Employee(
   phone            VARCHAR(15)           COMMENT 'Телефон',
   is_identified    BIT NULL DEFAULT 1    COMMENT 'Идентифицирован?',
 
-  FOREIGN KEY (office_Id)   REFERENCES Office(id)   COMMENT 'Внешний ключ на таблицу офис',
-  FOREIGN KEY (country_Id)  REFERENCES Country(id)  COMMENT 'Внешний ключ на таблицу страны'
+  FOREIGN KEY (office_id)   REFERENCES Office(id)   COMMENT 'Внешний ключ на таблицу офис',
+  FOREIGN KEY (country_id)  REFERENCES Country(id)  COMMENT 'Внешний ключ на таблицу страны'
  );
 COMMENT ON TABLE Employee IS 'Сотрудники';
 
 CREATE TABLE IF NOT EXISTS Document(
   id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
-  number           BIGINT NOT NULL       COMMENT 'Уникальный номер документа',
+  number           VARCHAR(15) NOT NULL  COMMENT 'Уникальный номер документа',
   doc_date         DATE                  COMMENT 'Дата создания',
 
-  FOREIGN KEY (employee_Id) REFERENCES Employee(id) COMMENT 'Внешний ключ на таблицу сотрудники',
-  FOREIGN KEY (doc_type_Id) REFERENCES Doc_Type(id) COMMENT 'Внешний ключ на таблицу тип документа'
+  FOREIGN KEY (employee_id) REFERENCES Employee(id) COMMENT 'Внешний ключ на таблицу сотрудники',
+  FOREIGN KEY (doc_type_id) REFERENCES Doc_Type(id) COMMENT 'Внешний ключ на таблицу тип документа'
  );
 COMMENT ON TABLE Document IS 'Документы';
 
-CREATE INDEX IX_Office_Organization_Id ON Office (org_Id);
-CREATE INDEX IX_Employee_Office_Id     ON Employee (office_Id);
-CREATE INDEX IX_Document_Employee_Id   ON Document (employee_Id);
-CREATE INDEX IX_Doc_Type_Document_Id   ON Doc_Type (document_Id);
-CREATE INDEX IX_Country_Employee_Id    ON Country (employee_Id);
+CREATE INDEX IX_Office_Organization_Id ON Office (org_id);
+CREATE INDEX IX_Employee_Office_Id     ON Employee (office_id);
+CREATE INDEX IX_Document_Employee_Id   ON Document (employee_id);
+CREATE INDEX IX_Doc_Type_Document_Id   ON Doc_Type (document_id);
+CREATE INDEX IX_Country_Employee_Id    ON Country (employee_id);

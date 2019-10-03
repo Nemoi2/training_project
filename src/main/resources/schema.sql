403839dd3619971c7fc32a6c14bd392691418c4c
Ilya Zaikov <Notherlight2@yandex.ru>
2019-10-03 19:42:16 +0300

    change FOREIGN KEY and update field type boolean

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Organization(
   kpp              VARCHAR(15) NOT NULL  COMMENT 'Код причины постановки на учёт',
   address          VARCHAR(50) NOT NULL  COMMENT 'Адрес',
   phone            VARCHAR(15)           COMMENT 'Телефон',
-  is_active        BIT NULL DEFAULT 1    COMMENT 'Активна ли органиация?'
+  is_active        BOOLEAN DEFAULT TRUE  COMMENT 'Активна ли органиация?'
  );
 COMMENT ON TABLE Organization IS 'Организация';
 
@@ -15,18 +15,17 @@ CREATE TABLE IF NOT EXISTS Office(
   id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
   name             VARCHAR(25) NOT NULL  COMMENT 'Имя офиса',
-  address          VARCHAR(50) NOT NULL  COMMENT 'Адрес'
+  address          VARCHAR(50) NOT NULL  COMMENT 'Адрес',
   phone            VARCHAR(15)           COMMENT 'Телефон',
-  is_active        BIT NULL DEFAULT 1    COMMENT 'Активен ли офис?',
-
-  FOREIGN KEY (org_id) REFERENCES Organization(id) COMMENT 'Внешний ключ на таблицу Организация'
+  is_active        BOOLEAN DEFAULT TRUE  COMMENT 'Активен ли офис?',
+  org_id           INTEGER NOT NULL      COMMENT 'Внешний ключ на организацию'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS Doc_Type(
   id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
-  name             VARCHAR(25) NOT NULL  COMMENT 'Имя документа',
+  name             VARCHAR(40) NOT NULL  COMMENT 'Имя документа',
   doc_code         VARCHAR(15)           COMMENT 'Код документа'
  );
 COMMENT ON TABLE Doc_Type IS 'Тип документа';
@@ -47,10 +46,9 @@ CREATE TABLE IF NOT EXISTS Employee(
   middle_name      VARCHAR(25)           COMMENT 'Отчество сотрудника',
   post             VARCHAR(25) NOT NULL  COMMENT 'Должность сотрудника',
   phone            VARCHAR(15)           COMMENT 'Телефон',
-  is_identified    BIT NULL DEFAULT 1    COMMENT 'Идентифицирован?',
-
-  FOREIGN KEY (office_id)   REFERENCES Office(id)   COMMENT 'Внешний ключ на таблицу офис',
-  FOREIGN KEY (country_id)  REFERENCES Country(id)  COMMENT 'Внешний ключ на таблицу страны'
+  is_identified    BOOLEAN DEFAULT TRUE  COMMENT 'Идентифицирован?',
+  office_id        INTEGER NOT NULL      COMMENT 'Внешний ключ на офис',
+  country_id       INTEGER NOT NULL      COMMENT 'Внешний ключ на страны'
  );
 COMMENT ON TABLE Employee IS 'Сотрудники';
 
@@ -59,14 +57,21 @@ CREATE TABLE IF NOT EXISTS Document(
   version          INTEGER NOT NULL      COMMENT 'Служебное поле hibernate',
   number           VARCHAR(15) NOT NULL  COMMENT 'Уникальный номер документа',
   doc_date         DATE                  COMMENT 'Дата создания',
-
-  FOREIGN KEY (employee_id) REFERENCES Employee(id) COMMENT 'Внешний ключ на таблицу сотрудники',
-  FOREIGN KEY (doc_type_id) REFERENCES Doc_Type(id) COMMENT 'Внешний ключ на таблицу тип документа'
+  doc_type_id      INTEGER NOT NULL      COMMENT 'Внешний ключ на страны'
  );
 COMMENT ON TABLE Document IS 'Документы';
 
 CREATE INDEX IX_Office_Organization_Id ON Office (org_id);
+ALTER TABLE Office ADD  FOREIGN KEY (org_id) REFERENCES Organization(id);
+
 CREATE INDEX IX_Employee_Office_Id     ON Employee (office_id);
+ALTER TABLE Employee ADD FOREIGN KEY (office_id)   REFERENCES Office(id);
+
 CREATE INDEX IX_Document_Employee_Id   ON Document (employee_id);
-CREATE INDEX IX_Doc_Type_Document_Id   ON Doc_Type (document_id);
-CREATE INDEX IX_Country_Employee_Id    ON Country (employee_id);
+ALTER TABLE Document ADD FOREIGN KEY (employee_id) REFERENCES Employee(id);
+
+CREATE INDEX IX_Document_Doc_Type_Id   ON Document (doc_type_id);
+ALTER TABLE Document ADD FOREIGN KEY (doc_type_id) REFERENCES Doc_Type(id);
+
+CREATE INDEX IX_Employee_Country_Id    ON Employee (country_id);
+ALTER TABLE Employee ADD FOREIGN KEY (country_id)  REFERENCES Country(id);

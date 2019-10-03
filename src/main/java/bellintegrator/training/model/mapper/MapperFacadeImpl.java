8c1f71c702de4803e60d3c96c07441894dc01f6e
Ilya Zaikov <Notherlight2@yandex.ru>
2019-10-03 19:47:30 +0300

    create mapper

---
+++ src/main/java/bellintegrator/training/model/mapper/MapperFacadeImpl.java
@@ -0,0 +1,44 @@
+package bellintegrator.training.model.mapper;
+
+import ma.glasnost.orika.MapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * {@inheritDoc}
+ */
+@Service
+public class MapperFacadeImpl implements MapperFacade {
+    private final MapperFactory mapperFactory;
+
+    @Autowired
+    public MapperFacadeImpl(MapperFactory mapperFactory) {
+        this.mapperFactory = mapperFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <S, D> D map(S sourceObject, Class<D> destinationClass) {
+        return mapperFactory.getMapperFacade().map(sourceObject, destinationClass);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <S, D> void map(S sourceObject, D destinationObject) {
+        mapperFactory.getMapperFacade().map(sourceObject, destinationObject);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <S, D> Set<D> mapAsSet(Iterable<S> source, Class<D> destinationClass) {
+        return mapperFactory.getMapperFacade().mapAsSet(source, destinationClass);
+    }
+}

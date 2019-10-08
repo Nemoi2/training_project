8dabebb040476ba2feadfc857d3ccd58eb57bb8a
Ilya Zaikov <Notherlight2@yandex.ru>
2019-10-08 17:32:43 +0300

    add method organizations

--- src/main/java/bellintegrator/training/controller/OrganizationController.java
+++ src/main/java/bellintegrator/training/controller/OrganizationController.java
@@ -1,13 +1,17 @@
 package bellintegrator.training.controller;
 
-import bellintegrator.training.model.Organization;
 import bellintegrator.training.service.OrganizationService;
+import bellintegrator.training.view.OrganizationsView;
 import bellintegrator.training.view.OrganizationView;
+import bellintegrator.training.view.View;
+import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -46,5 +50,16 @@ public class OrganizationController {
     @PostMapping("/update")
     public void updateOrganization(@RequestBody final OrganizationView organizationView) {
         organizationService.updateOrganization(organizationView);
+    }
+
+    @JsonView(View.class)
+    @ApiOperation(value = "Добавить новую организацию", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    @PostMapping("/list")
+    public List<OrganizationsView> organizations(@RequestBody final OrganizationsView organizationsView) {
+        return organizationService.organizations(organizationsView);
     }
 }

0d09fb861ccac2327e826553c7e4a1f562d6ce5f
Nemoi2 <notherlight2@yandex.ru>
2020-11-04 13:13:13 +0300

    add swagger

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-﻿Учебный проект. Ветка upgrade. Данный проект был усовершенствован с помощью Lombok и Spring Data.
+﻿Учебный проект. Ветка upgrade. Данный проект был усовершенствован с помощью Lombok и Spring Data. Был добавлен swagger.
 Репозиторий
 git - https://github.com/Nemoi2/training_project/tree/upgrade
--- src/main/java/bellintegrator/training/Application.java
+++ src/main/java/bellintegrator/training/Application.java
@@ -1,7 +1,7 @@
 package bellintegrator.training;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Основной файл приложения
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .run(args);
     }
 }
